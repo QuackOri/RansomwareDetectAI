e6290781da0ba0d1e33706cd31771776f06c9c52
QuackOri <wn0701@naver.com>
2023-11-03 19:43:39 +0900

    revise this_file_is.py

--- this_file_is.py
+++ this_file_is.py
@@ -18,6 +18,7 @@ for file in os.listdir(directory_path):
     # RDS
     api_result = RDS.is_ransom_by_api(cwd+'/models/r_api_model.pickle', file_path)
     sn_result = RDS.is_ransom_by_sectionname(cwd+'/models/r_new_sectionname_model.pickle', file_path)
+    pe_result = RDS.is_ransom_by_pe(cwd+'/models/r_api_model.pickle', file_path)
     # BDS
     b_api_result = BDS.is_malware_by_api(cwd+'/models/b_api_model.pickle', file_path)
     b_etrp_result = BDS.is_malware_by_entropy(cwd+'/models/b_entropy_model.pickle', file_path)
@@ -33,6 +34,10 @@ for file in os.listdir(directory_path):
         print('\033[31m' + "RDS-SN: " + "Ransom" + '\033[0m')
     else:
         print('\033[32m' + "RDS-SN: " + "It's okay" + '\033[0m')
+    if pe_result[0] == 1:
+        print('\033[31m' + "RDS-PE: " + "Ransom" + '\033[0m')
+    else:
+        print('\033[32m' + "RDS-PE: " + "It's okay" + '\033[0m')
     print()
     if b_api_result[0] == 1:
         print('\033[31m' + "BDS-API: " + "Malware" + '\033[0m')
