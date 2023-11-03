0384e9c100a3b8fea123a60e557da146dcb01985
QuackOri <wn0701@naver.com>
2023-11-03 19:40:20 +0900

    revise RDS api model

--- code/RDS.py
+++ code/RDS.py
@@ -13,7 +13,7 @@ def is_ransom_by_api(api_model_path, data_path):
 
     # data 전처리
     pre_data_by_api = [rp.pre_api(data_path)]
-
+    print(len(pre_data_by_api))
     api_df = list_to_df(pre_data_by_api, rp.api_columns)
     return api_model.predict(api_df)
 
@@ -29,12 +29,12 @@ def is_ransom_by_sectionname(sn_model_path, data_path):
     return sn_model.predict(sn_df)
 
 def is_ransom_by_pe(pe_model_path, data_path):
-    with open(pe_model_path, 'rb') as f:
-        pe_saved_dict = pickle.load(f)
-    pe_model = pe_saved_dict['model']
+  with open(pe_model_path, 'rb') as f:
+    pe_saved_dict = pickle.load(f)
+  pe_model = pe_saved_dict['model']
 
-    # data 전처리
-    pre_data_by_pe = [rp.pre_pe(data_path)]
+  # data 전처리
+  pre_data_by_pe = [rp.pre_pe(data_path)]
 
-    pe_df = list_to_df(pre_data_by_pe, rp.pe_columns)
-    return pe_model.predict(pe_df)
+  pe_df = list_to_df(pre_data_by_pe, rp.pe_columns)
+  return pe_model.predict(pe_df)
